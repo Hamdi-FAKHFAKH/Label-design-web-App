69991573f4cc02b6856f78c5542541f5da319c2e
Hamdi-FAKHFAKH <hamdi.fakhfakh1998@gmail.com>
2023-06-27 15:43:51 +0100

    ajouter un tableau de bord et résoudre le problème de vérification d'étiquette

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,43 @@
 import { Component, OnInit } from "@angular/core";
+import { DetailImpressionHttpService } from "../APT/DetailImpression/detailImpressionHttp.service";
+import { GestionProduitHttpService } from "../APT/GestionProduits/GestionProduitHttp.service";
 
 @Component({
   selector: "ngx-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  totalPrintedLabel;
+  totalRePrintedLabel;
+  totalRePrintedLabelwithSN;
+  totalProduit;
+  historiqueProduit;
+  constructor(
+    private detailImpressionHttpService: DetailImpressionHttpService,
+    private gestionProduitHttpService: GestionProduitHttpService
+  ) {}
+  async ngOnInit() {
+    this.totalPrintedLabel = (
+      await this.detailImpressionHttpService
+        .GetTotalEtiquettesImprimees()
+        .toPromise()
+    ).etiquettesImprimees[0].total;
+    //
+    this.totalRePrintedLabel = (
+      await this.detailImpressionHttpService
+        .GetTotalEtiquettesRéimprimees()
+        .toPromise()
+    ).etiquettesImprimees[0].total;
+    //
+    this.totalRePrintedLabelwithSN = (
+      await this.detailImpressionHttpService
+        .GetTotalEtiquettesRéimprimeesWithSN()
+        .toPromise()
+    ).etiquettesImprimees[0].total;
+    //
+    this.totalProduit = (
+      await this.gestionProduitHttpService.getTotalProduitNumber().toPromise()
+    ).produits[0].total;
+  }
 }
